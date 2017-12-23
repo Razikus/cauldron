6cad3b97c6005d44848a91adc74b9ffd5676d236
Adam Raźniewski <adam.razniewski@gmail.com>
2017-12-23 12:07:30 +0100

    cli

--- cauldron.py
+++ cauldron.py
@@ -1,1 +1,11 @@
-#Here will be script to get hosts from cauldron database
+import cli.app
+
+@cli.app.CommandLineApp
+def cauldron(app):
+    print app.params.list
+    pass
+
+cauldron.add_param("-l", "--list", help="lists for all avalaible hosts and groups", default = False, action = "store_true")
+
+if __name__ == "__main__":
+    cauldron.run()
