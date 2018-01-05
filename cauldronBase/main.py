28de7ebc700fa033a9db5dc50ae2c25a6a9b5148
Edge Developer <edge@debian-edge-develop>
2018-01-05 11:30:53 +0000

    add host to group etc

--- cauldronBase/main.py
+++ cauldronBase/main.py
@@ -35,13 +35,21 @@ def removeHost(host, group):
     return constructOKStatus()
 
 def removeHostFromBase(session, hostName, groupName = None):
-    session.add(host)
+    return "Not yet implemented"
 
 @app.route("/host/<host>/<group>", methods=["POST"])
 @app.route("/host/<host>", defaults = {'group' : None}, methods=["POST"])
 def addHost(host, group):
     session = SessionMaker()
     addHostToBase(session, hostName = host, groupName = group)
+    session.commit()
+    return constructOKStatus()
+
+
+@app.route("/group/<group>", methods = ["POST"])
+def addGroup(group):
+    session = SessionMaker()
+    addHostToBase(session, groupName = group);
     session.commit()
     return constructOKStatus()
 
@@ -64,14 +72,28 @@ def createDBSchema(engine):
 def getSession(sessionMaker):
     return sessionMaker();
 
-def addHostToBase(session, hostName, groupName = None):
-    host = CauldronHost(ip = hostName)
-    if groupName is not None:
+def addHostToBase(session, hostName = None, groupName = None):
+    if hostName is None and groupName is not None:
         group = CauldronGroup(name = groupName)
+        session.add(group)
+    elif hostName is not None and groupName is None:
+        host = CauldronHost(ip = hostName)
+        session.add(host)
+    elif hostName is not None and groupName is not None:
+        group = session.query(CauldronGroup).filter_by(name = groupName).first()
+        if(group is None):
+            addHostToBase(session, groupName = groupName)
+            return addHostToBase(session, hostName, groupName)
+        host = session.query(CauldronHost).filter_by(ip = hostName).first()
+        if(host is None):
+            addHostToBase(session, hostName = hostName)
+            return addHostToBase(session, hostName, groupName)
+
         assoc = CauldronGroupHostAssoc()
         assoc.group = group
+        assoc.host = host
         host.groups.append(assoc)
-    session.add(host)
+        session.add(host)
 
 def constructOKStatus():
     return '{ "status" : "OK"}'
